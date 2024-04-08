8e61a26a2534639efd8f85947cf9b697ec5a904e
thebe <thebe@DESKTOP-7076JJU>
2024-04-08 19:39:41 -0300

    Ejercicio 1 donde sacamos el promedio

--- PV_1/src/ar/edu/unju/fi/ejercicio1/Main_1.java
+++ PV_1/src/ar/edu/unju/fi/ejercicio1/Main_1.java
@@ -1,10 +1,18 @@
-package ar.edu.unju.fi.ejercicio1;
+package ar.edu.unju.fi.ejercicio1; // Reemplaza "tu.paquete" con el nombre de tu paquete
 
 public class Main_1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        // Declarar e inicializar las variables
+        int num1 = 8;
+        int num2 = 5;
+        int num3 = 1;
+        int num4 = 20;
+        int num5 = 9;
+        
+        // Calcular el promedio
+        double promedio = (num1 + num2 + num3 + num4 + num5) / 5.0;
+        
+        // Mostrar el resultado por consola
+        System.out.println("El promedio es: " + promedio);
+    }
 }
