4ef510886247856e4c2b74dcd0009eedb468f929
thebe <thebe@DESKTOP-7076JJU>
2024-04-08 21:04:03 -0300

    Ejercicio 2. Declarando variables, asignando valores y mostrando valores

--- PV_1/src/ar/edu/unju/fi/ejercicio2/Main_2.java
+++ PV_1/src/ar/edu/unju/fi/ejercicio2/Main_2.java
@@ -1,10 +1,42 @@
-package ar.edu.unju.fi.ejercicio2;
+package ar.edu.unju.fi.ejercicio2; // Reemplaza "tu.paquete" con el nombre de tu paquete
 
 public class Main_2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+      
+    	
+    	// Acá declaramos variables
+        String nombrePais;
+        int edadPersona;
+        double alturaEdificio;
+        double precioProducto;
+        String numeroTelefono;
+        double coseno;
+        
+        // Aquí asignamos los valores
+        nombrePais = "Argentina";
+        edadPersona = 30;
+        alturaEdificio = 100.5;
+        precioProducto = 5.99;
+        numeroTelefono = "123456789";
+        coseno = Math.cos(0.5);
+        
+        // Acá mostramos las variables por consola
+        System.out.println("País: " + nombrePais);
+        System.out.println("Edad: " + edadPersona);
+        System.out.println("Altura del edificio: " + alturaEdificio + " metros");
+        System.out.println("Precio del producto: $" + precioProducto);
+        System.out.println("Número de teléfono: " + numeroTelefono);
+        System.out.println("Coseno de 0.5: " + coseno);
+        System.out.println("Este es el ejercicio 2 resuelto");
+    }
 }
+
+
+		
+		
+		
+		
+		
+		
+		
+		
