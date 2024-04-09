ba89310d714bc98d597e70d43e5bcf743d7cc431
thebe <thebe@DESKTOP-7076JJU>
2024-04-08 22:20:09 -0300

    ejercicio 3 donde pedimos ingresar un numero al usuario y calculamos el multiplo dependiendo si es par o impar

--- PV_1/src/ar/edu/unju/fi/ejercicio3/Main_3.java
+++ PV_1/src/ar/edu/unju/fi/ejercicio3/Main_3.java
@@ -18,7 +18,7 @@ public class Main_3 {
             System.out.println("El triple del número es: " + (numero * 3));
         }
         
-        // aqui cerramos el ciclo scanner 
+        // aqui cerramos el ciclo scanner pa liberar recursos
         scanner.close();
     }
 }
