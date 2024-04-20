8c978afff1bedf100de1a3c216ca8ab583668f2a
thebe <thebe@DESKTOP-7076JJU>
2024-04-20 17:21:37 -0300

    ejercicio 11

---
+++ PV_1/src/ar/edu/unju/fi/ejercicio11/main/Main_11.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.ejercicio11.main;
+
+public class Main_11 {
+    public static void main(String[] args) {
+        int i = 40;
+        int j = 2;
+
+        for (; i >= 0; i--, j++) {
+            System.out.print((i * j) + " ");
+        }
+    }
+}
